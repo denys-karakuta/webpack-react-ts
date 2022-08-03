958421d3d846ba2db4c3f2fd383d630d58adf96d
Denys Karakuta <denys.karakuta@gmail.com>
2022-08-03 18:01:46 +0300

    basic webpack config and step 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import './styles.css'
+import logo from './img/logo.svg'
+
 const App = () => {
     return (
         <div>
-            App.tsx
+            <img src={logo} alt=""/>
+            App.tsx. FINALLY!
         </div>
     );
 };
 
 export default App;
+
