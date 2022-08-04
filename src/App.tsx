88d50be60db1e8e769ba85a7d5641f401ab4beb1
Denys Karakuta <denys.karakuta@gmail.com>
2022-08-04 09:48:07 +0300

    divided webpack config / step 7

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ const App = () => {
     return (
         <div>
             <img src={logo} alt=""/>
-            App.tsx. FINALLY!
+            App.tsx. FINALLY! <br/> <br/>
+            {process.env.NODE_ENV} <br/>
+            {process.env.name}
         </div>
     );
 };
 
-export default App;
-
+export default App
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,59 +1,7 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { merge } = require('webpack-merge')
+const commonConfig = require('./webpack.common.js')
 
-module.exports = env => {
-    const isDevelopment = env.env !== 'production'
-    return {
-        entry: path.resolve(__dirname, '..', './src/index.tsx'),
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js']
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(ts|js)x?$/,
-                    exclude: /node_modules/,
-                    use: [
-                        {
-                            loader: require.resolve('babel-loader'),
-                            options: {
-                                plugins: [
-                                    // isDevelopment && require.resolve('react-refresh/babel')
-                                ]
-                            }
-                        }
-                    ]
-                },
-                {
-                    test: /\.css$/,
-                    use: ['style-loader', 'css-loader']
-                },
-                {
-                    test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-                    type: 'asset/resource',
-                },
-                {
-                    test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-                    type: 'asset/inline',
-                },
-            ]
-        },
-        output: {
-            path: path.resolve(__dirname, '..', './build'),
-            filename: 'bundle.js'
-        },
-        // devServer: {
-        //     contentBase: path.resolve(__dirname, '..', './build'),
-        //     hot: true
-        // },
-        mode: 'development',
-        plugins: [
-            new CleanWebpackPlugin(),
-            new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, '..', './src/index.html')
-            }),
-            // isDevelopment && new ReactRefreshWebpackPlugin()
-        ]
-    }
+module.exports = ({ env }) => {
+    const envConfig = require(`./webpack.${env}.js`)
+    return merge(commonConfig, envConfig)
 }
