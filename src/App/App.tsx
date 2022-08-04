f9e8342060ad77778c79347ddfc4633ea7a722cd
Denys Karakuta <denys.karakuta@gmail.com>
2022-08-04 19:08:55 +0300

    step 8

---
+++ src/App/App.tsx
@@ -0,0 +1,18 @@
+import './styles.css'
+import logo from '../assets/logo.svg'
+import Counter from '../Counter/Counter';
+
+const App = () => {
+    return (
+        <div>
+            <img src={logo} alt="Logo" width='720'/>
+
+            <p>NODE_ENV = {process.env.NODE_ENV}</p>
+            <p>Config name = {process.env.name}</p>
+
+            <Counter />
+        </div>
+    );
+};
+
+export default App
---
+++ src/Counter/Counter.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    const increment = () => {
+        setCount(count => count + 1)
+    }
+
+    return (
+        <div>
+            <button onClick={increment}>+</button>
+            <p>Count: {count}</p>
+        </div>
+    );
+};
+
+export default Counter;
