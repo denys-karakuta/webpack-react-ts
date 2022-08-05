05c6ff69a886cad91d702bddef196b7a88a28b4d
Denys Karakuta <denys.karakuta@gmail.com>
2022-08-05 17:03:29 +0300

    fix .gitattributes

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,8 @@
 module.exports = {
-	semi: true,
-	trailingComma: 'es5',
-	singleQuote: true,
-	jsxSingleQuote: true,
-	printWidth: 80,
-	useTabs: false,
-	tabWidth: 2,
-	endOfLine: 'auto',
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  endOfLine: 'auto',
 };
