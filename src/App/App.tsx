6972e59a61d5090f04c5c13fbde0423bf7f1fcb5
Denys Karakuta <denys.karakuta@gmail.com>
2022-08-05 17:20:13 +0300

    fix

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,16 +3,16 @@ import logo from '../assets/logo.svg';
 import Counter from '../Counter/Counter';
 
 const App = () => {
-	return (
-		<div>
-			<img src={logo} alt='Logo' width='720' />
+    return (
+        <div>
+            <img src={logo} alt='Logo' width='720' />
 
-			<p>NODE_ENV = {process.env.NODE_ENV}</p>
-			<p>Config name = {process.env.name}</p>
+            <p>NODE_ENV = {process.env.NODE_ENV}</p>
+            <p>Config name = {process.env.name}</p>
 
-			<Counter />
-		</div>
-	);
+            <Counter />
+        </div>
+    );
 };
 
 export default App;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,6 +2,6 @@ const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 
 module.exports = ({ env }) => {
-	const envConfig = require(`./webpack.${env}.js`);
-	return merge(commonConfig, envConfig);
+    const envConfig = require(`./webpack.${env}.js`);
+    return merge(commonConfig, envConfig);
 };
